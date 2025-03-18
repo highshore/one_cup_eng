a47c0e59c416667c33179eb65429f4061693eece
Kyle SK Kim <ksk@Kyles-MacBook-Pro.local>
2025-03-19 01:03:24 +0900

    basic setup

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBC62vsKGQqdgpyC9RugoHEfh9UcRi2SMA",
+  authDomain: "one-cup-eng.firebaseapp.com",
+  projectId: "one-cup-eng",
+  storageBucket: "one-cup-eng.firebasestorage.app",
+  messagingSenderId: "615807178262",
+  appId: "1:615807178262:web:9a96a5f0d94ae628d74737"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
